6392181092b1e553ff492b2cfd42e87c95767230
nenavizhu leto <nenavizhu leto>
2023-10-08 21:13:58 -0700

    chore: moving to svelte

--- www/src/routes/+page.svelte
+++ www/src/routes/+page.svelte
@@ -3,12 +3,7 @@
 </script>
 <div class="flex justify-between items-center mt-16 mb-10">
   <h1 class="text-4xl font-bold">Обращения</h1>
-  <a
-    hx-get="/system/task/new"
-    hx-target="body"
-    hx-swap="beforeend"
-    class="inline-flex bg-primary text-white py-4 px-8 rounded-xl items-center gap-4 font-semibold transition-all hover:bg-hover"
-  >
+  <a class="inline-flex bg-primary text-white py-4 px-8 rounded-xl items-center gap-4 font-semibold transition-all hover:bg-hover">
     Новое обращение
   </a>
 </div>
