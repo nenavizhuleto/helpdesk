d693721753d5c4ce2ae7859a9ccbb8630fb05215
nenavizhu leto <nenavizhu.leto@gmail.com>
2023-10-15 10:04:19 -0700

    chore(api/users): read and create user's tasks by specific url
    To access user's tasks: GET `/api/v2/users/:user_id/tasks`
    To create task from user's: POST `/api/v2/users/:user_id/tasks`

--- api/users.go
+++ api/users.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
@@ -17,7 +18,8 @@ func SetUsersRoutes(path string, router fiber.Router) {
 	users.Put("/", UpdateUser)
 	users.Delete("/", DeleteUser)
 
-	// users.Get("/:id/tasks", GetUserTasks)
+	users.Get("/:id/tasks", GetUserTasks)
+	users.Post("/:id/tasks", CreateUserTask)
 }
 
 func GetUsers(c *fiber.Ctx) error {
@@ -43,13 +45,12 @@ func GetUser(c *fiber.Ctx) error {
 }
 
 func CreateUser(c *fiber.Ctx) error {
-	var user models.User
-	if err := c.BodyParser(&user); err != nil {
+	var user = models.NewUser()
+	if err := c.BodyParser(user); err != nil {
 		return fmt.Errorf("createUser: %w", err)
 	}
 
-	user.ID = uuid.NewString()
-	if err := user.Create(c.IP()); err != nil {
+	if err := user.Create(); err != nil {
 		return fmt.Errorf("createUser: %w", err)
 	}
 
@@ -80,4 +81,52 @@ func DeleteUser(c *fiber.Ctx) error {
 	}
 
 	return c.JSON(user)
+}
+
+func GetUserTasks(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	user, err := models.UserByID(id)
+	if err != nil {
+		return err
+	}
+
+	tasks, err := models.TasksByUser(user)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(tasks)
+
+}
+
+func CreateUserTask(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	dev, err := models.DeviceGetByIP(c.IP())
+	if err != nil {
+		return fmt.Errorf("user: %w", err)
+	}
+
+	var task = models.NewTask()
+
+	if err := c.BodyParser(&task); err != nil {
+		return fmt.Errorf("user: %w", err)
+	}
+
+	user, err := models.UserByID(id)
+	if err != nil {
+		return err
+	}
+
+	task.User = *user
+	task.Company = dev.Company
+	task.Branch = dev.Branch
+
+	if err := task.Create(); err != nil {
+		return err
+	}
+
+	return c.JSON(task)
+
 }
