7d4dd71b6e8dceec3f0eb92245e0c320ae7b7cc5
nenavizhu leto <nenavizhu.leto@gmail.com>
2023-10-16 05:56:29 +0000

    Megaplan integration

---
+++ api/megaplan.go
@@ -0,0 +1,40 @@
+package api
+
+import (
+	"application/megaplan"
+	"application/models/v2"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+
+func HandleMegaplanEvent(c *fiber.Ctx) error {
+	var event megaplan.TaskEvent
+	if err := c.BodyParser(&event); err != nil {
+		log.Println(err)
+		return c.SendStatus(500)
+	}
+
+	if event.Model != "Task" {
+		return c.SendStatus(200)
+	}
+
+	log.Printf("Event: %#v", event)
+
+	dto := event.Data
+	task, err := models.TaskByID(dto.ID)
+	if err != nil {
+		log.Printf("event: %s", err.Error())
+		return c.SendStatus(200)
+	}
+
+	task.Status = dto.Status
+
+	if err := task.Update(); err != nil {
+		log.Println(err)
+		return c.SendStatus(500)
+	}
+
+	return c.SendStatus(200)
+}
--- api/tasks.go
+++ api/tasks.go
@@ -4,11 +4,7 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/google/uuid"
-
-	"application/auth/v1"
-	"application/megaplan"
-	"application/models/v1"
+	"application/models/v2"
 )
 
 func SetTasksRoutes(path string, router fiber.Router) {
@@ -29,16 +25,11 @@ func GetTasks(c *fiber.Ctx) error {
 }
 
 func CreateTask(c *fiber.Ctx) error {
-	var task models.Task
+	var task = models.NewTask()
 	if err := c.BodyParser(&task); err != nil {
 		return fmt.Errorf("createTask: %w", err)
 	}
 
-	task.BeforeCreate = func(t *models.Task) error {
-		fmt.Println("TODO: Create Task in Megaplan")
-		t.ID = uuid.NewString()
-		return nil
-	}
 
 	if err := task.Create(); err != nil {
 		return fmt.Errorf("createTask: %w", err)
@@ -71,59 +62,4 @@ func DeleteTask(c *fiber.Ctx) error {
 	}
 
 	return c.JSON(task)
-}
-
-func GetTask(c *fiber.Ctx) error {
-	i := auth.GetIdentity(c)
-	if i == nil {
-		return models.ErrNotIdentified
-	}
-
-	task_id := c.Params("id")
-
-	task, err := models.GetTaskForUser(i.User.ID, task_id)
-	if err != nil {
-		return models.ErrEntityNotFound
-	}
-
-	if relevantTask, err := megaplan.MP.HandleFetchTaskUpdates(task); err != nil {
-		return models.ErrMegaplan
-	} else {
-		if relevantTask.Status != task.Status {
-			task.Status = relevantTask.Status
-			models.UpdateTaskForUser(i.User.ID, task)
-		} else {
-			task.Comments = relevantTask.Comments
-		}
-	}
-
-	return c.JSON(task)
-}
-
-func CreateTask1(c *fiber.Ctx) error {
-	i := auth.GetIdentity(c)
-	if i == nil {
-		return models.ErrNotIdentified
-	}
-	var task models.Task
-	if err := c.BodyParser(&task); err != nil {
-		return models.ErrAPIInvalidRequestBody
-	}
-
-	createdTask, err := megaplan.MP.HandleCreateTask(i, &task)
-	if err != nil {
-		return models.ErrMegaplan
-	}
-
-	// FIXME: Replacing returned task from megaplan with original subject
-	createdTask.Subject = task.Subject
-
-	if err := models.SaveTaskForUser(i.User.ID, createdTask); err != nil {
-		return models.ErrDatabase
-	}
-	if err = createdTask.Prettify(); err != nil {
-		return fiber.NewError(500, err.Error())
-	}
-
-	return c.JSON(createdTask)
 }
