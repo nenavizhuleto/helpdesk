4247d54a5d4eee8c6bcf7d4f45b38dfd1d7811d9
nenavizhu leto <nenavizhu.leto@gmail.com>
2023-10-19 00:40:46 -0700

    chore(routes/tasks): get identity and user tasks on page load

--- frontend/src/routes/tasks/+page.server.ts
+++ frontend/src/routes/tasks/+page.server.ts
@@ -1,5 +1,16 @@
+import { getIdentity } from '$lib/api/auth';
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { getUserTasks } from '$lib/api/tasks';
 
 export const load = (async () => {
-    return {};
+	const identity = await getIdentity();
+	if (!identity) {
+		throw redirect(300, "/register");
+	}
+	const tasks = await getUserTasks(identity.user.id);
+	return {
+		identity: identity,
+		tasks: tasks,
+	};
 }) satisfies PageServerLoad;
