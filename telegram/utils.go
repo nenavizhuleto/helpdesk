8b3a310086f4fe740ef0672646d463613c26269d
nenavizhu leto <nenavizhu.leto@gmail.com>
2023-10-25 02:10:40 -0700

    feat(telegram): I DONT KNOW HOW TO GIT REBASE AND MERGE THIS F-ING BRANCHES ANYMORE

---
+++ telegram/utils.go
@@ -0,0 +1,32 @@
+package telegram
+
+import (
+	"fmt"
+
+	tg "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func (t *TelegramNotificator) send(c tg.Chattable) (tg.Message, error) {
+	return t.b.Send(c)
+}
+
+func (t *TelegramNotificator) SendMessage(update tg.Update, text string) (tg.Message, error) {
+	msg := tg.NewMessage(update.Message.Chat.ID, text)
+	return t.b.Send(msg)
+}
+
+func (t *TelegramNotificator) SendError(update tg.Update, err error) (tg.Message, error) {
+	msg := tg.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Error occured: %s", err.Error()))
+	return t.b.Send(msg)
+}
+
+func (t *TelegramNotificator) SendMarkdown(update tg.Update, markdown string, args ...any) (tg.Message, error) {
+
+	text := markdown
+	if len(args) > 0 {
+		text = fmt.Sprintf(markdown, args...)
+	}
+	msg := tg.NewMessage(update.Message.Chat.ID, text)
+	msg.ParseMode = tg.ModeMarkdown
+	return t.b.Send(msg)
+}
