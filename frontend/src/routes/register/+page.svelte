ad8492efc923c1cc18235b4db2ad6eb092c2b2f3
nenavizhu leto <nenavizhu.leto@gmail.com>
2023-10-27 01:46:19 -0700

    fix: CLOSES #13

--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -6,7 +6,7 @@
 	import type { Writable } from "svelte/store";
 	import type { Identity } from "$lib/api/types";
 	import type { ActionData } from "./$types";
-    import { redirect } from "@sveltejs/kit";
+	import { redirect } from "@sveltejs/kit";
 	export let data: PageData;
 
 	const identity: Writable<Identity> = getContext("identity");
@@ -14,62 +14,56 @@
 
 	$: () => {
 		if (form?.success) {
-			throw redirect(302, "/")
+			throw redirect(302, "/");
 		}
-	}
+	};
 </script>
 
-{#if $identity}
-	<p>Already authorized</p>
-{/if}
-
-{#if form?.success}
-	<p>Successfully registered</p>
-{:else if !form?.success}
-	<p>{form?.field} is empty</p>
-{/if}
 <div class="flex gap-24 justify-center max-w-4xl mx-auto mt-56">
-  <div class="self-center">
-    <div class="text-4xl font-bold leading-10 mb-8">Вы почти у цели!</div>
-    <div class="text-xl font-normal leading-tight">Чтобы начать пользоваться приложением, пожалуйста, пройдите быструю и простую процедуру регистрации</div>
-  </div>
-  <Card class="w-full max-w-md">
-  	<form class="flex flex-col space-y-6" action="?/register" method="post">
-  		<h3 class="text-xl font-medium text-gray-900 dark:text-white">
-  			Регистрация в системе
-  		</h3>
-  		<Label class="space-y-2">
-  			<span>Имя</span>
-  			<Input
-  				type="text"
-  				size="lg"
-  				name="firstName"
-  				placeholder="Нателла"
-  				required
-  			/>
-  		</Label>
-  		<Label class="space-y-2">
-  			<span>Фамилия</span>
-  			<Input
-  				type="text"
-  				size="lg"
-  				name="lastName"
-  				placeholder="Наумова"
-  				required
-  			/>
-  		</Label>
-  		<Label class="space-y-2">
-  			<span>Внутренний номер</span>
-  			<Input
-  				type="text"
-  				size="lg"
-  				name="phone"
-  				placeholder="1234"
-  				required
-  			/>
-  		</Label>
-  		<Button type="submit" color="blue" class="w-full">Продолжить</Button
-  		>
-  	</form>
-  </Card>  
+	<div class="self-center">
+		<div class="text-4xl font-bold leading-10 mb-8">Вы почти у цели!</div>
+		<div class="text-xl font-normal leading-tight">
+			Чтобы начать пользоваться приложением, пожалуйста, пройдите быструю
+			и простую процедуру регистрации
+		</div>
+	</div>
+	<Card class="w-full max-w-md">
+		<form class="flex flex-col space-y-6" action="?/register" method="post">
+			<h3 class="text-xl font-medium text-gray-900 dark:text-white">
+				Регистрация в системе
+			</h3>
+			<Label class="space-y-2">
+				<span>Имя</span>
+				<Input
+					type="text"
+					size="lg"
+					name="firstName"
+					placeholder="Нателла"
+					required
+				/>
+			</Label>
+			<Label class="space-y-2">
+				<span>Фамилия</span>
+				<Input
+					type="text"
+					size="lg"
+					name="lastName"
+					placeholder="Наумова"
+					required
+				/>
+			</Label>
+			<Label class="space-y-2">
+				<span>Внутренний номер</span>
+				<Input
+					type="text"
+					size="lg"
+					name="phone"
+					placeholder="1234"
+					required
+				/>
+			</Label>
+			<Button type="submit" color="blue" class="w-full">Продолжить</Button
+			>
+		</form>
+	</Card>
 </div>
