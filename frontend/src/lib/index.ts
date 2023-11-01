9410d36a41f105916706cac826046cd448917a05
fairyplace <fairyplacesss@gmail.com>
2023-11-01 15:28:04 +0500

    chore:
    add format date, scroll to bottom functions

--- frontend/src/lib/index.ts
+++ frontend/src/lib/index.ts
@@ -1,2 +1,22 @@
 // place files you want to import through the `$lib` alias in this folder.
 
+export function shiftDate(str) {
+  if (str.length == 1) {
+    return "0" + str;
+  }
+  return str;
+}
+
+export function formatDate(date) {
+  let tm = new Date(date);
+  let day = shiftDate(tm.getDate().toString());
+  let month = shiftDate((tm.getMonth() + 1).toString());
+  let year = tm.getFullYear().toString().slice(-2);
+  let hour = shiftDate(tm.getHours().toString());
+  let minutes = shiftDate(tm.getMinutes().toString());
+  return `${day}.${month}.${year} ${hour}:${minutes}`;
+}
+
+export const  scrollToBottom = async (node) => {
+  node.scroll({ top: node.scrollHeight, behavior: 'smooth' });
+}; 
